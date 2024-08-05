e045a5fd6023a493eef1d933cd7dcb5612e23d8f
Oluwadare Oluwatobiloba <tobidechamp15@gmail.com>
2024-08-05 23:26:33 +0100

    reason

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import About from "./components/About";
 import Hero from "./components/Hero";
 import HeroImage from "./components/HeroImage";
 import Navbar from "./components/Navbar";
+import Reason from "./components/Reason";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       </div>
       <HeroImage />
       <About />
+      <Reason />
     </>
   );
 }
